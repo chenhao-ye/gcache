ce256ce9c127d18638a02f769357dcc028a50b7d
Chenhao Ye <ye.chenhao.ych@gmail.com>
2023-11-24 09:14:47 -0600

    update parse and plot for benchmark

--- scripts/parse_perf.py
+++ scripts/parse_perf.py
@@ -2,31 +2,45 @@ import logging
 import os
 import pandas as pd
 
-results_dir = "results"
 
-df_all = None
+def parse():
+    results_dir = "results"
 
-for subdir in os.listdir(results_dir):
-    if not subdir.startswith("sr"):
-        logging.warning(f"Unknown subdirectory: {results_dir}/{subdir}")
-        continue
-    df = pd.read_csv(f"{results_dir}/{subdir}/perf.csv",
-                     header=0,
-                     index_col=[
-                         "sample_shift", "workload", "num_blocks",
-                         "zipf_theta", "rand_seed"
-                     ],
-                     skipinitialspace=True)
-    if df_all is None:
-        df_all = df
-    else:
-        df_all = pd.concat([df_all, df])
+    df_all = None
 
-df_raw = df_all.sort_values(
-    by=["sample_shift", "workload", "num_blocks", "zipf_theta"])
-df_raw.to_csv(f"{results_dir}/perf_raw.csv")
+    for subdir in os.listdir(results_dir):
+        if not subdir.startswith("sr"):
+            logging.warning(f"Unknown subdirectory: {results_dir}/{subdir}")
+            continue
+        df = pd.read_csv(f"{results_dir}/{subdir}/perf.csv",
+                         header=0,
+                         index_col=[
+                             "sample_shift", "workload", "num_blocks",
+                             "zipf_theta", "rand_seed"
+                         ],
+                         skipinitialspace=True)
+        df_all = df if df_all is None else pd.concat([df_all, df])
 
-df_group = df_all.groupby(
-    by=["sample_shift", "workload", "num_blocks", "zipf_theta"])
-df_group.mean().to_csv(f"{results_dir}/perf_mean.csv")
-df_group.std().to_csv(f"{results_dir}/perf_std.csv")
+    df_all["ghost_cost_us_per_op"] = \
+        (df_all["ghost_us"] - df_all["baseline_us"]) / df_all["num_ops"]
+    df_all["sampled_cost_us_per_op"] = \
+        (df_all["sampled_us"] - df_all["baseline_us"]) / df_all["num_ops"]
+
+    df_raw = df_all.sort_values(
+        by=["sample_shift", "workload", "num_blocks", "zipf_theta"])
+    df_raw.to_csv(f"{results_dir}/perf_raw.csv")
+
+    df_group = df_all.groupby(
+        by=["sample_shift", "workload", "num_blocks", "zipf_theta"])
+
+    df_mean = df_group.mean().reset_index()
+    df_std = df_group.std().reset_index()
+
+    df_mean.to_csv(f"{results_dir}/perf_mean.csv")
+    df_std.to_csv(f"{results_dir}/perf_std.csv")
+
+    return df_mean, df_std
+
+
+if __name__ == "__main__":
+    parse()
--- scripts/plot_perf.py
+++ scripts/plot_perf.py
@@ -2,8 +2,9 @@ import pandas as pd
 import math
 import matplotlib
 import matplotlib.pyplot as plt
-from typing import List, Tuple
+from typing import Tuple
 from plot_util import *
+from parse_perf import parse
 
 results_dir = "results"
 sample_shift_list = [3, 4, 5, 6, 7, 8]
@@ -11,81 +12,95 @@ sample_shift_list = [3, 4, 5, 6, 7, 8]
 matplotlib.rcParams['pdf.fonttype'] = 42
 matplotlib.rcParams['ps.fonttype'] = 42
 
-fig, (ax_err, ax_cost) = get_subplots(nrows=1, ncols=2)
-ax_err.spines[['right', 'top']].set_visible(False)
-ax_cost.spines[['right', 'top']].set_visible(False)
-df_mean = pd.read_csv(f"{results_dir}/perf_mean.csv",
-                        header=0,
-                        index_col=False)
-df_std = pd.read_csv(f"{results_dir}/perf_std.csv",
-                        header=0,
-                        index_col=False)
 
-for wl, ws, theta, name in [
-    ("unif", 1024 * 1024 * 1024 / 4096, 0, "unif_s1G"),
-    ("zipf", 1024 * 1024 * 1024 / 4096, 0.99, "zipf_s1G_z0.99"),
-    ("zipf", 2 * 1024 * 1024 * 1024 / 4096, 0.5, "zipf_s2G_z0.5"),
-]:
-    make_curve(
-        ax_err,
-        df_mean,
-        x_col="sample_shift",
-        y_col="avg_err",
-        x_range=sample_shift_list,
-        filters={
-            "workload": wl,
-            "num_blocks": ws,
-            "zipf_theta": theta,
-        },
-        y_trans=lambda x: x * 100,  # unit: %
-        df_err=df_std,
-        name=name)
-    make_curve(
-        ax_cost,
-        df_mean,
-        x_col="sample_shift",
-        y_col="sampled_cost_uspop",
-        x_range=sample_shift_list,
-        filters={
-            "workload": wl,
-            "num_blocks": ws,
-            "zipf_theta": theta,
-        },
-        y_trans=lambda x: x * 1000,  # unit: us -> ns
-        df_err=df_std,
-        name=name)
-ax_err.set_ylim([0, 4])
-ax_cost.set_ylim([0, 100])
+def load_data() -> Tuple[pd.DataFrame, pd.DataFrame]:
+    df_mean = pd.read_csv(f"{results_dir}/perf_mean.csv",
+                          header=0,
+                          index_col=False)
+    df_std = pd.read_csv(f"{results_dir}/perf_std.csv",
+                         header=0,
+                         index_col=False)
+    return df_mean, df_std
 
-ax_err.set_xticks(sample_shift_list,
-                    [f"1/{2**x}" for x in sample_shift_list],
-                    fontsize=8)
-ax_err.set_xlabel("Sample rate")
-ax_cost.set_xticks(sample_shift_list,
-                    [f"1/{2**x}" for x in sample_shift_list],
-                    fontsize=8)
-ax_cost.set_xlabel("Sample rate")
 
-ticks_err = [0, 1, 2, 3, 4]
-ticks_cost = [0, 25, 50, 75, 100]
-for ax, ticks in [(ax_err, ticks_err), (ax_cost, ticks_cost)]:
-    ax.set_yticks(ticks, [f"{t}" for t in ticks], rotation=90)
+def plot(df_mean: pd.DataFrame, df_std: pd.DataFrame):
+    fig, (ax_err, ax_cost) = get_subplots(nrows=1, ncols=2)
+    ax_err.spines[['right', 'top']].set_visible(False)
+    ax_cost.spines[['right', 'top']].set_visible(False)
 
-ax_err.set_ylabel("Error ratio (%)")
-ax_cost.set_ylabel("Overhead (ns/block)")
+    for wl, ws, theta, name in [
+        ("unif", 1024 * 1024 * 1024 / 4096, 0, "unif_s1G"),
+        ("zipf", 1024 * 1024 * 1024 / 4096, 0.99, "zipf_s1G_z0.99"),
+        ("zipf", 2 * 1024 * 1024 * 1024 / 4096, 0.5, "zipf_s2G_z0.5"),
+    ]:
+        make_curve(
+            ax_err,
+            df_mean,
+            x_col="sample_shift",
+            y_col="avg_err",
+            x_range=sample_shift_list,
+            filters={
+                "workload": wl,
+                "num_blocks": ws,
+                "zipf_theta": theta,
+            },
+            y_trans=lambda x: x * 100,  # unit: %
+            df_err=df_std,
+            name=name)
+        make_curve(
+            ax_cost,
+            df_mean,
+            x_col="sample_shift",
+            y_col="sampled_cost_us_per_op",
+            x_range=sample_shift_list,
+            filters={
+                "workload": wl,
+                "num_blocks": ws,
+                "zipf_theta": theta,
+            },
+            y_trans=lambda x: x * 1000,  # unit: us -> ns
+            df_err=df_std,
+            name=name)
+    ax_err.set_ylim([0, 4])
+    ax_cost.set_ylim([0, 100])
 
-fig.set_tight_layout({"pad": 0.05, "w_pad": 0.05, "h_pad": 0.05})
-print("Save figure as `ghost_perf.png`")
-fig.savefig("ghost_perf.png")
-print("Save figure as `ghost_perf.pdf`")
-fig.savefig("ghost_perf.pdf")
-plt.close(fig)
+    ax_err.set_xticks(sample_shift_list,
+                      [f"1/{2**x}" for x in sample_shift_list],
+                      fontsize=8)
+    ax_err.set_xlabel("Sample rate")
+    ax_cost.set_xticks(sample_shift_list,
+                       [f"1/{2**x}" for x in sample_shift_list],
+                       fontsize=8)
+    ax_cost.set_xlabel("Sample rate")
 
-print("Save legend as `ghost_perf_legend.pdf`")
-legend_fig = make_legend(["zipf_s1G_z0.99", "unif_s1G", "zipf_s2G_z0.5"])
+    ticks_err = [0, 1, 2, 3, 4]
+    ticks_cost = [0, 25, 50, 75, 100]
+    for ax, ticks in [(ax_err, ticks_err), (ax_cost, ticks_cost)]:
+        ax.set_yticks(ticks, [f"{t}" for t in ticks], rotation=90)
 
-print("Legend saved to `ghost_perf_legend.png`")
-legend_fig.savefig("ghost_perf_legend.png")
-print("Legend saved to `ghost_perf_legend.pdf")
-legend_fig.savefig("ghost_perf_legend.pdf")
-plt.close(legend_fig)
+    ax_err.set_ylabel("Error ratio (%)")
+    ax_cost.set_ylabel("Overhead (ns/block)")
+
+    fig.set_tight_layout({"pad": 0.05, "w_pad": 0.05, "h_pad": 0.05})
+    print("Save figure as `ghost_perf.png`")
+    fig.savefig("ghost_perf.png")
+    print("Save figure as `ghost_perf.pdf`")
+    fig.savefig("ghost_perf.pdf")
+    plt.close(fig)
+
+    print("Save legend as `ghost_perf_legend.pdf`")
+    legend_fig = make_legend(["zipf_s1G_z0.99", "unif_s1G", "zipf_s2G_z0.5"])
+
+    print("Legend saved to `ghost_perf_legend.png`")
+    legend_fig.savefig("ghost_perf_legend.png")
+    print("Legend saved to `ghost_perf_legend.pdf")
+    legend_fig.savefig("ghost_perf_legend.pdf")
+    plt.close(legend_fig)
+
+
+if __name__ == "__main__":
+    df_mean, df_std = parse()
+    # alternatively, directly load the data
+    # df_mean, df_std = load_data()
+
+    plot(df_mean, df_std)
